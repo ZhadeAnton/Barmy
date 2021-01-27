eb749e5b128c8db457dc9faad260af158934d059
ElGutiar <el12guitar@gmail.com>
2021-01-27 23:47:05 +0300

    working on header

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './scss/main.scss';
 
-console.log('barmy!');
+const hamburger = document.querySelector('.header__hamburger');
+const header = document.querySelector('.header');
+const hasFade = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
+
+hamburger.addEventListener('click', function () {
+  if (!header.classList.contains('open')) {
+    console.log(body);
+    header.classList.add('open');
+    body.classList.remove('noscroll');
+    hasFade.forEach((element) => {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+      console.log(body);
+    });
+  } else {
+    header.classList.remove('open');
+    body.classList.add('noscroll');
+    hasFade.forEach((element) => {
+      element.classList.add('fade-out');
+      element.classList.remove('fade-in');
+    });
+  }
+});
